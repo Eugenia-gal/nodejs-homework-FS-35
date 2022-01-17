532c51c9b35b15e5601c39ad2be07a1a782dd647
Eugenia-gal <galevbor12@gmail.com>
2022-01-17 15:25:12 +0200

    set up router.get  '/'

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,28 @@
 import { Router } from "express";
+import model from "../../model/index.js";
 
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contacts = await model.listContacts();
+  console.log(contacts);
+  res.status(200).json(contacts);
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.get("/:contactId", async (_req, res, _next) => {
+//   res.json({ message: "template message" });
+// });
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.post("/", async (_req, res, _next) => {
+//   res.json({ message: "template message" });
+// });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.delete("/:contactId", async (_req, res, _next) => {
+//   res.json({ message: "template message" });
+// });
 
-router.patch("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.patch("/:contactId", async (_req, res, _next) => {
+//   res.json({ message: "template message" });
+// });
 
 export default router;
