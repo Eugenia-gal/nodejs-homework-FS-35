9693325dc0b215c80d5e75632e154f56de71c05c
Eugenia-gal <galevbor12@gmail.com>
2022-01-24 14:47:30 +0200

    change default port

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,12 @@
 import app from "../app.js";
-import db from "../lib/db";
+import db from "../lib/db.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 db.then(() => {
   app.listen(PORT, () => {
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
-}).catch((err) => {});
-console.log(`Server not running. ${err.message}`);
+}).catch((err) => {
+  console.log(`Server not running. ${err.message}`);
+});
