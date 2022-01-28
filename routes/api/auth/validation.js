339859d13b08cf5ddff040c475565006a9891e48
Eugenia-gal <galevbor12@gmail.com>
2022-01-28 19:29:47 +0200

    fix validation of users

--- routes/api/auth/validation.js
+++ routes/api/auth/validation.js
@@ -1,8 +1,5 @@
 import Joi from "joi";
-// import mongoose from "mongoose";
 import { HttpCode } from "../../../lib/constants.js";
-
-// const { Types } = mongoose;
 
 const createSchema = Joi.object({
   name: Joi.string().optional(),
@@ -15,17 +12,6 @@ const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
-
-// const updateFavoriteSchema = Joi.object({
-//   favorite: Joi.boolean().required(),
-// });
-
-// export async function validateId(req, res, next) {
-//   if (!Types.ObjectId.isValid(req.params.id)) {
-//     return res.status(400).json({ message: "Invalid ObjectId" });
-//   }
-//   next();
-// }
 
 export async function validateCreating(req, res, next) {
   try {
@@ -52,16 +38,3 @@ export async function validateLogin(req, res, next) {
   }
   next();
 }
-
-// export const validateUpdatingFavorite = async (req, res, next) => {
-//   try {
-//     await updateFavoriteSchema.validateAsync(req.body);
-//   } catch (err) {
-//     const [{ type }] = err.details;
-//     if (type === "object.missing") {
-//       return res.status(400).json({ message: "missing field favorite" });
-//     }
-//     return res.status(400).json({ message: err.message });
-//   }
-//   next();
-// };
