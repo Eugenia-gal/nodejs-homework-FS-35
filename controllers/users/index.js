e36362c82de9de965d2ce7de28162632c39d7665
Eugenia-gal <galevbor12@gmail.com>
2022-01-31 20:11:58 +0200

    add sending verify email at signup and user verify

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,8 +1,8 @@
 import { HttpCode } from "../../lib/constants.js";
+import Users from "../../repository/users.js";
 import {
   UploadFileService,
   LocalFileStorage,
-  //   CloudFileStorage,
 } from "../../service/file-storage/index.js";
 
 const uploadAvatar = async (req, res, next) => {
@@ -17,4 +17,25 @@ const uploadAvatar = async (req, res, next) => {
     .json({ status: "success", code: HttpCode.OK, data: { avatarUrl } });
 };
 
-export { uploadAvatar };
+const verifyUser = async (req, res, next) => {
+  const verifyToken = req.params.verificationToken;
+  const userFromToken = await Users.getUserByVerifyToken(verifyToken);
+
+  if (userFromToken) {
+    await Users.updateVerify(userFromToken.id, true);
+    return res.status(HttpCode.OK).json({
+      status: "success",
+      code: HttpCode.OK,
+      data: { message: "Verification successful" },
+    });
+  }
+  res.status(HttpCode.BAD_REQUEST).json({
+    status: "Not Found",
+    code: HttpCode.BAD_REQUEST,
+    data: { message: "User not found" },
+  });
+};
+
+const repeatEmailForUserVerify = async (req, res, next) => {};
+
+export { uploadAvatar, verifyUser, repeatEmailForUserVerify };
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { uploadAvatar } from "../../../controllers/users/index.js";
+import {
+  uploadAvatar,
+  verifyUser,
+  repeatEmailForUserVerify,
+} from "../../../controllers/users/index.js";
 import guard from "../../../middlewares/guard.js";
 import { upload } from "../../../middlewares/upload.js";
 
 const router = new Router();
 
 router.patch("/avatar", guard, upload.single("avatar"), uploadAvatar);
+
+router.get("/verify/:verificationToken", verifyUser);
+
+router.post("/verify", repeatEmailForUserVerify);
 
 export default router;
